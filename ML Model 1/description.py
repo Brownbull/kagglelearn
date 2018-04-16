33b448e25a8977091f42c7907024f6cf9d33d34f
brownbull <carcamo.gabriel@gmail.com>
2018-04-16 12:41:39 -0300

    model with MAE done

---
+++ ML Model 1/description.py
@@ -0,0 +1,17 @@
+# All columns
+# Id,MSSubClass,MSZoning,LotFrontage,LotArea,Street,Alley,LotShape,LandContour,Utilities,LotConfig,LandSlope,Neighborhood,Condition1,Condition2,BldgType,HouseStyle,OverallQual,OverallCond,YearBuilt,YearRemodAdd,RoofStyle,RoofMatl,Exterior1st,Exterior2nd,MasVnrType,MasVnrArea,ExterQual,ExterCond,Foundation,BsmtQual,BsmtCond,BsmtExposure,BsmtFinType1,BsmtFinSF1,BsmtFinType2,BsmtFinSF2,BsmtUnfSF,TotalBsmtSF,Heating,HeatingQC,CentralAir,Electrical,1stFlrSF,2ndFlrSF,LowQualFinSF,GrLivArea,BsmtFullBath,BsmtHalfBath,FullBath,HalfBath,BedroomAbvGr,KitchenAbvGr,KitchenQual,TotRmsAbvGrd,Functional,Fireplaces,FireplaceQu,GarageType,GarageYrBlt,GarageFinish,GarageCars,GarageArea,GarageQual,GarageCond,PavedDrive,WoodDeckSF,OpenPorchSF,EnclosedPorch,3SsnPorch,ScreenPorch,PoolArea,PoolQC,Fence,MiscFeature,MiscVal,MoSold,YrSold,SaleType,SaleCondition,SalePrice
+
+import pandas as pd
+
+melbourne_file_path = 'train.csv'
+melbourne_data = pd.read_csv(melbourne_file_path)
+# Stats from dataset
+# print(melbourne_data.describe())
+# # get columns only
+# print(melbourne_data.columns)
+# # Get Prices
+# melbourne_price_data = melbourne_data.SalePrice
+# print(melbourne_price_data.head())
+columns_of_interest = ['LotArea', 'MasVnrArea']
+two_columns_of_data = melbourne_data[columns_of_interest]
+print(two_columns_of_data.describe())
--- ML Model 1/model.py
+++ ML Model 1/model.py
@@ -1,5 +1,50 @@
 import pandas as pd
 
+# Input Data
 melbourne_file_path = 'train.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
-print(melbourne_data.describe())
+# print(melbourne_data.columns)
+
+# Predictions Location
+y = melbourne_data.SalePrice
+
+# Predictors Def
+# melbourne_predictors = ['BedroomAbvGr', 'FullBath', 'LotArea', 'TotalBsmtSF',
+#                         'YearBuilt']
+melbourne_predictors = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF',
+                        'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+
+# Predictors Location
+X = melbourne_data[melbourne_predictors]
+
+# Model Fit imports
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+
+from sklearn.model_selection import train_test_split
+# split data into training and validation data, for both predictors and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+
+# Define Model
+melbourne_model = DecisionTreeRegressor()
+
+# Fit Model
+melbourne_model.fit(train_X, train_y)
+# melbourne_model.fit(X, y)
+# print("Making predictions for the following 5 houses:")
+# print(X.head())
+# print("The predictions are")
+# print(melbourne_model.predict(X.head()))
+
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
+
+
+
+
+
+
